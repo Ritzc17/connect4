e957390c5a1204e83db26b189f55201b44c3e2d7
Niko <nburdy@gmail.com>
2026-09-19 16:46:10 -0700

    Add input validation

--- connect4.py
+++ connect4.py
@@ -19,15 +19,27 @@ print("  1   2   3   4   5   6   7")
 current_player = "X"
 
 while True:
-	column = int(input(f"Player {current_player}, choose a column: "))
+	try:
+		column = int(input(f"Player {current_player}, choose a column: "))
+	except ValueError:
+		print("Please enter a number.")
+		continue
+	if column <1 or column > 7:
+		print("Please choose a column from 1 to 7.")
+		continue
+
+	piece_placed = False
+
 	for row in range(ROWS - 1, -1, -1):
 		if board[row][column - 1] == " ":
 			board[row][column - 1] = current_player
+			piece_placed = True
 			break
-	if current_player == "X":
-		current_player = "O"
-	else:
-		current_player = "X"
+	if piece_placed:
+		if current_player == "X":
+			current_player = "O"
+		else:
+			current_player = "X"
 
 	for row in board:
 		print("|", end="")
