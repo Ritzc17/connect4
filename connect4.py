49f4949a99c249c66ce04136a0d593a23bcbb7a4
Niko <nburdy@gmail.com>
2026-09-19 15:26:47 -0700

    Add Connect Four board

--- connect4.py
+++ connect4.py
@@ -1,3 +1,17 @@
+ROWS = 6
+COLS = 7
+
+board = []
+for row in range(ROWS):
+	board.append([" "] * COLS)
+
 print("CONNECT FOUR")
-print("============")
-print("Coming soon!")
+print("==============")
+
+for row in board:
+	print("|", end="")
+	for cell in row:
+		print(f" {cell} |", end="")
+	print()
+
+print("  1   2   3   4   5   6   7")
