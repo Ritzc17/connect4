76088e3fabac05a4b5a781b4e7f5feff1b60b4b9
Niko <nburdy@gmail.com>
2026-09-19 16:20:25 -0700

    Add alternating player turns

--- connect4.py
+++ connect4.py
@@ -15,3 +15,24 @@ for row in board:
 	print()
 
 print("  1   2   3   4   5   6   7")
+
+current_player = "X"
+
+while True:
+	column = int(input(f"Player {current_player}, choose a column: "))
+	for row in range(ROWS - 1, -1, -1):
+		if board[row][column - 1] == " ":
+			board[row][column - 1] = current_player
+			break
+	if current_player == "X":
+		current_player = "O"
+	else:
+		current_player = "X"
+
+	for row in board:
+		print("|", end="")
+		for cell in row:
+			print(f" {cell} |", end="")
+		print()
+
+	print("  1   2   3   4   5   6   7")
